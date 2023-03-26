8dd2c520d8b2fbedc50b6b8fc1091b1f4b91fbca
Nilotpal Maitra <nilotpalm2015@gmail.com>
2023-03-26 17:41:08 +0530

    first commit

---
+++ practice1.py
@@ -0,0 +1,18 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+#Perform operations 
+
+#addition operation 
+def p_add(sum):
+    para = a+b
+    print("Sum of a and b is: ",para)
+
+def p_diff(subtract):
+    para= a-b
+    print("Difference of a and b is: ",para)
+
+def p_multiply(multiplication):
+    para= a*b
+    print("Multiplication of a and b is: ",para)
+    
+
